c7850979557dc4f8b2e7e9f88ebdc8ee87f4af75
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-04 12:57:36 -0500

    add desgin Form

--- src/app/components/form-reactive/form-reactive.component.ts
+++ src/app/components/form-reactive/form-reactive.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-form-reactive',
   templateUrl: './form-reactive.component.html',
-  styleUrls: ['./form-reactive.component.css']
+  styleUrls: ['./form-reactive.component.css'],
 })
-export class FormReactiveComponent {
-
-}
+export class FormReactiveComponent {}
